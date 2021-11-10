d85767e665828a365ded6b74e4843073292bd84b
Agata Figura <agata.figura1@gmail.com>
2021-11-10 22:18:10 +0100

    .

--- javascript/form.js
+++ javascript/form.js
@@ -228,4 +228,12 @@ submitButton.addEventListener("click", showPopUp)
 
 function showPopUp() {
   document.querySelector("#wrapper").classList.remove("hidden");
+  document.querySelector("main").classList.add("hidden");
 }
+
+const name = document.querySelector(".name");
+const nameValue = document.querySelector("#fullname").value;
+
+console.log(nameValue);
+
+name.innerHTML = nameValue;
