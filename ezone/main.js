fa7eeffaa61a07ac2d29f3b92016ef5222836904
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-11-10 18:59:19 +0100

    form almost done

--- ezone/main.js
+++ ezone/main.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
-import "/style.scss";
-document.querySelector("#signup").addEventListener("click", function () {
-  location.href = "form.html";
-});
+"use strict;";
 
-=======
-import '/style.scss'
->>>>>>> 8710790b676f0e9a1981dbe5b3342a9301a7340d
+import data from "./data.json";
+console.log(data);
+import "/style.scss";
+
+
+// gotoForm();
+
 //validate repeated password
 
 var passwordinput = document.getElementById("password"),
@@ -103,15 +103,15 @@ const eyeToggle = document.querySelector("#eye");
 eyeToggle.addEventListener("click", toggleEye);
 
 function toggleEye() {
-    if (eyeToggle.classList.contains("fa-eye-slash")) {
-        eyeToggle.classList.remove("fa-eye-slash");
-        eyeToggle.classList.add("fa-eye");
-        document.querySelector("#password").setAttribute("type", "text");
-    } else {
-        eyeToggle.classList.remove("fa-eye");
-        eyeToggle.classList.add("fa-eye-slash");
-        document.querySelector("#password").setAttribute("type", "password");
-    }
+  if (eyeToggle.classList.contains("fa-eye-slash")) {
+    eyeToggle.classList.remove("fa-eye-slash");
+    eyeToggle.classList.add("fa-eye");
+    document.querySelector("#password").setAttribute("type", "text");
+  } else {
+    eyeToggle.classList.remove("fa-eye");
+    eyeToggle.classList.add("fa-eye-slash");
+    document.querySelector("#password").setAttribute("type", "password");
+  }
 }
 
 const eyeToggle2 = document.querySelector("#eye2");
@@ -119,13 +119,105 @@ const eyeToggle2 = document.querySelector("#eye2");
 eyeToggle2.addEventListener("click", toggleEye2);
 
 function toggleEye2() {
-    if (eyeToggle2.classList.contains("fa-eye-slash")) {
-        eyeToggle2.classList.remove("fa-eye-slash");
-        eyeToggle2.classList.add("fa-eye");
-        document.querySelector("#repeat-password").setAttribute("type", "text");
-    } else {
-        eyeToggle2.classList.remove("fa-eye");
-        eyeToggle2.classList.add("fa-eye-slash");
-        document.querySelector("#repeat-password").setAttribute("type", "password");
-    }
+  if (eyeToggle2.classList.contains("fa-eye-slash")) {
+    eyeToggle2.classList.remove("fa-eye-slash");
+    eyeToggle2.classList.add("fa-eye");
+    document.querySelector("#repeat-password").setAttribute("type", "text");
+  } else {
+    eyeToggle2.classList.remove("fa-eye");
+    eyeToggle2.classList.add("fa-eye-slash");
+    document.querySelector("#repeat-password").setAttribute("type", "password");
+  }
 }
+
+// second section
+
+const list = document.querySelector("#mySelect");
+const array = data.games;
+
+array.forEach((element) => {
+  console.log(element);
+  let options = document.createElement("option");
+  list.appendChild(options);
+  console.log(options.value);
+  options.setAttribute("value", `${element}`);
+  options.textContent = `${element}`;
+ list.firstElementChild.setAttribute("disabled", true);
+
+});
+
+document.querySelectorAll("option").forEach((option) => {
+  option.addEventListener("click", (e) => {
+    if (data.games.includes(e.target.value)) {
+      e.target.classList.add("check");
+      const b = document.createElement("button");
+      b.textContent = e.target.value;
+      b.setAttribute("value", e.target.value);
+      document.querySelector(".chosen").appendChild(b);
+      console.log(e.target.value);
+      let x = document.querySelector("#mySelect");
+      x.remove(x.selectedIndex);
+    }
+  });
+});
+
+
+const list2 = document.querySelector("#mySelect2");
+const array2 = data.types;
+
+array2.forEach((element) => {
+  console.log(element);
+  let options2 = document.createElement("option");
+  list2.appendChild(options2);
+  console.log(options2.value);
+  options2.setAttribute("value", `${element}`);
+  options2.textContent = `${element}`;
+ list2.firstElementChild.setAttribute("disabled", true);
+
+});
+
+document.querySelectorAll("option").forEach((option) => {
+  option.addEventListener("click", (e) => {
+    if (data.types.includes(e.target.value)) {
+      e.target.classList.add("check");
+      const c = document.createElement("button");
+      c.textContent = e.target.value;
+      c.setAttribute("value", e.target.value);
+      document.querySelector(".chosen2").appendChild(c);
+      console.log(e.target.value);
+      let y = document.querySelector("#mySelect2");
+      y.remove(y.selectedIndex);
+    }
+  });
+});
+
+
+
+const list3 = document.querySelector("#mySelect3");
+const array3 = data.areas;
+
+array3.forEach((element) => {
+  console.log(element);
+  let options3 = document.createElement("option");
+  list3.appendChild(options3);
+  console.log(options3.value);
+  options3.setAttribute("value", `${element}`);
+  options3.textContent = `${element}`;
+ list3.firstElementChild.setAttribute("disabled", true);
+
+});
+
+document.querySelectorAll("option").forEach((option) => {
+  option.addEventListener("click", (e) => {
+    if (data.areas.includes(e.target.value)) {
+      e.target.classList.add("check");
+      const d = document.createElement("button");
+      d.textContent = e.target.value;
+      d.setAttribute("value", e.target.value);
+      document.querySelector(".chosen3").appendChild(d);
+      console.log(e.target.value);
+      let z = document.querySelector("#mySelect3");
+      z.remove(z.selectedIndex);
+    }
+  });
+});
---
+++ ezone/modules/animations.js
@@ -0,0 +1,6 @@
+import "/style.scss";
+
+
+document.querySelector("#goform").addEventListener("click", function () {
+  location.href = "form.html";
+});
